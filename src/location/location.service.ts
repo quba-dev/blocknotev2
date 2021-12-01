c020d7ae7b9ddfbcf0c850232741040f0d3941c8
quba <macbookquba@icloud.com>
2021-12-01 16:23:29 +0600

    location>date

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -3,7 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Location } from './location.entity';
 import { Repository } from 'typeorm';
 import { CreateLocationDto } from './dto/create-location.dto';
-import { Activity } from '../activity/activity.entity';
+import {type} from "os";
+
 
 @Injectable()
 export class LocationService {
@@ -49,18 +50,27 @@ export class LocationService {
 
   async findByLocation(id: number) {
     const locationById = await this.locationService.findOne(id,{relations:["activities"]})
+
     if (!locationById) {
       throw new HttpException('there is no such location', HttpStatus.NOT_FOUND)
     }
     const currentActivity= locationById.activities
+    // let obj = currentActivity.find( o => o.start_date)
+    for (let i of currentActivity){
+      console.log((i.start_date).toDateString())
+      console.log((i.end_date).toDateString())
+      console.log((i.name))
+    }
 
     return currentActivity
 
 
   }
 
-  async findLocationByTime(location: number, start_date: string, end_date: string){
-    console.log('Hello motherfucker')
+  async findLocationByTime(id: number, start_date: string, end_date: string){
+    const location = await this.locationService.findOne(id)
+
+    console.log(location)
   }
 
 }
